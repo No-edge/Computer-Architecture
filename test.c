ff7c08936ede55fcf88469ee494df8af235bf1ab
No-edge <61647929+No-edge@users.noreply.github.com>
2020-04-01 22:05:51 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,50 +1,183 @@
+#include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "vector.h"
-
-int main(int argc, char **argv) {
-    vector_t *v;
-
-    printf("Calling vector_new()\n");
-    v = vector_new();
-
-    printf("Calling vector_delete()\n");
-    vector_delete(v);
-
-    printf("vector_new() again\n");
-    v = vector_new();
-
-    printf("These should all return 0 (vector_get()): ");
-    printf("%d ", vector_get(v, 0));
-    printf("%d ", vector_get(v, 1));
-    printf("%d\n", vector_get(v, 2));
-
-    printf("Doing a bunch of vector_set()s\n");
-    vector_set(v, 0, 98);
-    vector_set(v, 11, 15);
-    vector_set(v, 15, -23);
-    vector_set(v, 24, 65);
-    vector_set(v, 500, 3);
-    vector_set(v, 12, -123);
-    vector_set(v, 15, 21);
-    vector_set(v, 25, 43);
-
-    printf("These should be equal:\n");
-    printf("98 = %d\n", vector_get(v, 0));
-    printf("15 = %d\n", vector_get(v, 11));
-    printf("65 = %d\n", vector_get(v, 24));
-    printf("-123 = %d\n", vector_get(v, 12));
-    printf("21 = %d\n", vector_get(v, 15));
-    printf("43 = %d\n", vector_get(v, 25));
-    printf("0 = %d\n", vector_get(v, 23));
-    printf("0 = %d\n", vector_get(v, 1));
-    printf("0 = %d\n", vector_get(v, 501));
-    printf("3 = %d\n", vector_get(v, 500));
-
-    vector_delete(v);
-
-    printf("Test complete.\n");
-
+/* You may find implementing this function helpful */
+int bitSigner(unsigned int field UNUSED, unsigned int size UNUSED) {
+    /* YOUR CODE HERE */
     return 0;
 }
+
+/* You may find implementing this function handy */
+int get_imm_operand(Instruction inst UNUSED) {//deal with jalr ...
+    /* YOUR CODE HERE */
+    //I-type
+    unsigned int a = inst.itype.imm;
+    int b=0x800;
+    b=b&a;
+    if(b==0x800)
+    {
+        a-=1;
+        a=~a;
+    }
+    //把二进制转换成十进制
+    int sum=0;
+    for(int i=0;i<12;i++)
+    {
+        b=0x1;
+        b=b<<i;
+        int c=a&b;
+        if(c==b)
+        {
+            sum+=c;
+        }
+    }
+    if(b==0x800)
+    {
+        sum=-1*sum;
+    }
+            
+    return sum;
+            
+}//输入为I-type
+
+/* Remember that the offsets should return the offset in BYTES */
+
+int get_branch_offset(Instruction inst UNUSED) {//deal with beq...
+    /* YOUR CODE HERE */
+    //SB-type
+
+    //拼接，所得的imm的值为imm
+    unsigned int a1,a2,a3,a4;
+    unsigned int imm5,imm7;
+    imm5=inst.sbtype.imm5;
+    imm7=inst.sbtype.imm7;
+    a3=imm5&0x1;
+    a3=a3<<10;
+    a1=imm5>>1;
+    int per=1<<6;
+    a4=imm7&per;
+    a2=imm7-a4;
+    a2=a2<<4;
+    a4=a4<<5;
+    unsigned int imm;
+    imm=a1+a2+a3+a4;
+
+    //判断imm的正负
+    if(a4==0x800)
+    {
+        imm-=1;
+        imm=~imm;
+    }
+    //转换成十进制
+    int sum=0;
+    for(int i=0;i<12;i++)
+    {
+        int b=0x1;
+        b=b<<i;
+        int c=imm&b;
+        if(c==b)
+        {
+            sum+=c;
+        }   
+    }
+
+    if(a4==0x800)
+    {
+        sum=-1*sum;
+    }
+            
+    return sum; 
+}
+
+int get_jump_offset(Instruction inst UNUSED) {//deal with jal
+    /* YOUR CODE HERE */
+    //UJ-type
+    unsigned int a1,a2,a3,a4;
+    unsigned int imm=inst.ujtype.imm;
+    a4=imm&0x80000;
+    a2=imm&0x100;
+    a3=imm&0xff;
+    a1=imm&0x7fe00;
+    a1=a1>>9;
+    a2=a2<<2;
+    a3=a3<<11;
+    imm=a1+a2+a3+a4;
+
+    //判断imm的正负
+    if(a4==0x80000)
+    {        
+        imm-=1;
+        imm=~imm;
+    }
+    //转换成十进制
+    int sum=0;
+    for(int i=0;i<12;i++)
+    {    
+        int b=0x1;
+        b=b<<i;
+        int c=imm&b;
+        if(c==b)
+        {
+            sum+=c;
+        }   
+    }
+
+    if(a4==0x80000)
+    {
+        sum=-1*sum;
+    }
+                
+    return sum; 
+}
+
+int get_store_offset(Instruction inst UNUSED) {//deal with sw,sb,sh...
+    /* YOUR CODE HERE */
+    //s-type
+    unsigned int a2=inst.stype.imm7;
+    unsigned int a1=inst.stype.imm5;
+    a2=a2<<5;
+    unsigned int imm=a1+a2;
+    //判断imm的正负
+    int a=0x800;
+    a=a&imm;
+    if(a==0x800)
+    {
+        imm-=1;
+        imm=~imm;
+    }
+    int sum=0;
+    for(int i=0;i<12;i++)
+    {
+        int b=0x1;
+        b=b<<i;
+        int c=imm&b;
+        if(c==b)
+        {
+            sum+=c;
+        }   
+    }
+
+    if(a==0x800)
+    {
+        sum=-1*sum;
+    }
+        
+    return sum;
+ 
+}
+
+void handle_invalid_instruction(Instruction inst) {
+    printf("Invalid Instruction: 0x%08x\n", inst.bits); 
+}
+
+void handle_invalid_read(Address address) {
+    printf("Bad Read. Address: 0x%08x\n", address);
+    exit(-1);
+}
+
+void handle_invalid_write(Address address) {
+    printf("Bad Write. Address: 0x%08x\n", address);
+    exit(-1);
+}
+
